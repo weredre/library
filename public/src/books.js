e2b85e0185af36241c6d68ba85cb7cfb4d9fa2e1
Andre Heslop <andreheslop@gmail.com>
2021-12-07 15:10:58 -0500

    updated books and home

--- public/src/books.js
+++ public/src/books.js
@@ -1,16 +1,39 @@
 function findAuthorById(authors, id) {
-  let found = authors.find((author)=> author.id === id);
+    let found = authors.find((author)=> author.id === id);
   return found;
 }
 
 function findBookById(books, id) {
   let found = books.find((book)=> book.id === id);
   return found;
+
 }
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const borrowedBooks = books.filter((book) => book.borrows[0].returned === false);
+  const returnedBooks = books.filter((book) => book.borrows[0].returned === true);
+  return [borrowedBooks, returnedBooks];
 
-function getBorrowersForBook(book, accounts) {}
+}
+
+function getBorrowersForBook(book, accounts) {
+   const borrowArr = [];
+  const borrowLog = book.borrows;
+  const accountIds = accounts.reduce((accu, account) => {
+    accu[account.id] = account;
+    return accu;
+  }, {});
+  for (let index in borrowLog){
+    for (let jndex in accountIds) {
+      if (accountIds[jndex].id === borrowLog[index].id) {
+        const { returned } = borrowLog[index];
+        accountIds[jndex]["returned"] = returned;
+        borrowArr.push(accountIds[jndex]);
+      }
+    }
+  }
+  return borrowArr.slice(0, 10);
+}
 
 module.exports = {
   findAuthorById,
--- public/src/home.js
+++ public/src/home.js
@@ -1,14 +1,69 @@
-function getTotalBooksCount(books) {}
+function getTotalBooksCount(books) {
+  return books.length;
+}
 
-function getTotalAccountsCount(accounts) {}
+function getTotalAccountsCount(accounts) {
+  return accounts.length;
+}
 
-function getBooksBorrowedCount(books) {}
+function getBooksBorrowedCount(books) {
+  let borrowedBooks = books.filter((book) => {
+    let borrowsArrZ = book.borrows[0];
+    return borrowsArrZ.returned === false;
+  });
+  return borrowedBooks.reduce((acc, value) => (acc += 1), 0);
+}
 
-function getMostCommonGenres(books) {}
+//helper function for Function 4 & 6
+function sortandSplice(array) {
+  return array.sort((obj1, obj2) => obj2.count - obj1.count).splice(0, 5);
+}
 
-function getMostPopularBooks(books) {}
+function getMostCommonGenres(books) {
+  const mapBooks = books.map((book) => book.genre);
+  const genreCount = mapBooks.reduce((acc, genre) => {
+    if (!acc[genre]) {
+      acc[genre] = 0;
+    }
+    acc[genre]++;
+    return acc;
+  }, {});
 
-function getMostPopularAuthors(books, authors) {}
+  const correctFormat = Object.keys(genreCount).map((key) => ({
+    name: key,
+    count: genreCount[key],
+  }));
+  return sortandSplice(correctFormat);
+}
+
+function getMostPopularBooks(books) {
+  const booksCounted = [];
+  for (let index in books) {
+    const bookTitle = books[index].title;
+    const bookBorrowsCount = books[index].borrows.length;
+    const titleAndCount = { name: bookTitle, count: bookBorrowsCount };
+    booksCounted.push(titleAndCount);
+  }
+  booksCounted.sort((bookA, bookB) => bookB.count - bookA.count);
+  return booksCounted.splice(0, 5);
+}
+
+function getMostPopularAuthors(books, authors) {
+  const popAuthorArr = books.reduce((accu, book) => {
+    const doesExist = accu.find((objInAcc) => objInAcc.authorId === book.authorId); //check the accu if an author obj has already been defined
+    const bookAuthor = authors.find((author) => author.id === book.authorId);
+    if (!doesExist) {
+      accu.push({
+        name: `${bookAuthor.name.first} ${bookAuthor.name.last}`,
+        count: book.borrows.length,
+      });
+    } else {
+      doesExist.count += book.borrows.length;
+    }
+    return accu;
+  }, []);
+  return sortandSplice(popAuthorArr);
+}
 
 module.exports = {
   getTotalBooksCount,
